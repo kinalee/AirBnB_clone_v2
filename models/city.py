895b5f8dd811ddb6526817b6be6af7e944f13906
Kina Lee <kina.lee@holbertonschool.com>
2017-03-21 03:33:23 +0000

    models updated

--- models/city.py
+++ models/city.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
 from models import *
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 
-class City(BaseModel):
-    state_id = ""
-    name = ""
+class City(BaseModel, Base):
+
+    __tablename__ = "cities"
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
 
     def __init__(self, *args, **kwargs):
         if len(args) > 0:
--- models/user.py
+++ models/user.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
 from models import *
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 
 class User(BaseModel):
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
+    places = relationship("Place", backref="user")
 
     def __init__(self, *args, **kwargs):
         if len(args) > 0:
