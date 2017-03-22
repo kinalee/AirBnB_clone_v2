ea98c4a8ad4618604d808d1f062a68fb97d286b3
Kina Lee <kina.lee@holbertonschool.com>
2017-03-22 08:34:43 +0000

    fixed

--- models/engine/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-
-if os.getenv('HBNB_TYPE_STORAGE') == "db":
-    from models.engine import db_storage
-    storage = db_storage.DBStorage()
-else:
-    from models.engine import file_storage
-    storage = file_storage.FileStorage()
-storage.reload()
