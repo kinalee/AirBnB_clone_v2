c7ba42a381069f5bb6a9c0cfe5cac2b00c3c9769
Kina Lee <kina.lee@holbertonschool.com>
2017-03-29 19:00:11 +0000

    6 fixed

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
 
-import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb')
 def return_hbnb():
     return 'HBNB'
+
 
 @app.route('/c/<text>')
 def cisfun(text):
     text = text.replace('_', ' ')
     return 'C {:s}'.format(text)
+
 
 @app.route('/python/')
 @app.route('/python/<text>')
@@ -24,25 +27,29 @@ def python(text='is cool'):
     text = text.replace('_', ' ')
     return 'Python {:s}'.format(text)
 
-@app.route('/number/<n>')
+
+@app.route('/number/<int:n>')
 def onlyint(n):
-    if int(n):
-        return '{:d} is a number'.format(int(n))
+        return '{:d} is a number'.format(n)
 
-@app.route('/number_template/<n>')
+
+@app.route('/number_template/<int:n>')
 def onlyinttemp(n):
-    if int(n):
-        return render_template("5-number.html", n = int(n))
+        return render_template("5-number.html", n=n)
 
-@app.route('/number_odd_or_even/<n>')
+
+@app.route('/number_odd_or_even/<int:n>')
 def evenorodd(n):
+    """
     if int(n):
         if int(n) % 2 == 0:
-            return render_template("6-number_odd_or_even.html", n = int(n),
+            return render_template("6-number_odd_or_even.html", n=int(n),
                                    evenodd="even")
         else:
-            return render_template("6-number_odd_or_even.html", n = int(n),
-                                   evenodd="odd")
+            return render_template("6-number_odd_or_even.html", n=int(n),
+                                    evenodd="odd")
+    """
+    return render_template("6-number_odd_or_even.html", n=n)
 
 
 if __name__ == '__main__':
